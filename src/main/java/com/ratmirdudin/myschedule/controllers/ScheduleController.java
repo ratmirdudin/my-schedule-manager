3ee38b9d983c352bfbfbd793cbe5cf1781b8feec
Ratmir <ratmirdudin@mail.ru>
2022-08-10 17:15:51 +0600

    create schedule controller

---
+++ src/main/java/com/ratmirdudin/myschedule/controllers/ScheduleController.java
@@ -0,0 +1,12 @@
+package com.ratmirdudin.myschedule.controllers;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/v1/schedule")
+public class ScheduleController {
+
+}
