aab4cab9f04c5772ec312ee685f4b6e0ad2edd17
Ratmir <ratmirdudin@mail.ru>
2022-08-10 15:51:24 +0600

    init

---
+++ src/test/java/com/ratmirdudin/myschedule/MyScheduleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ratmirdudin.myschedule;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyScheduleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
